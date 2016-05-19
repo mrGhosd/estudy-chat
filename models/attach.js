f14095f4b8b11a70b6996faedff6900083cf9008
Vadim Sokoltsov <vforvad@gmail.com>
2016-05-19 03:07:29 +0300

    remove useless staff from attach model

--- models/attach.js
+++ models/attach.js
@@ -6,20 +6,6 @@ var fileName;
 
 var Attach = bookshelf.model('Attach', {
   tableName: 'attaches',
-  constructor: function() {
-    var objectExists = Object.keys(arguments).length > 0;
-    if (objectExists) {
-      fileName = arguments['0']['file'];
-      // console.log(arguments);
-    }
-    bookshelf.Model.apply(this, arguments);
-  },
-  // initialize: function() {
-  //   var keys = Object.keys(this.attributes).length;
-  //   if (keys > 0) {
-  //     console.log(this.attributes);
-  //   }
-  // },
   attachable: function() {
     return this.morphTo('attachable', 'User', 'Message');
   },
